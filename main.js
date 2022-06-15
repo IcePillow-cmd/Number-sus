03628ae1fc4d6ca8a8d5c40179245a43a9a73fdc
ColdPillow <poplizardo4321@gmail.com>
2022-06-15 19:11:44 +0800

    Added condition to check if input is number

--- main.js
+++ main.js
@@ -7,19 +7,26 @@ function addRange(start, range=10) {
 
 function showWarning(field) {
     console.clear();
-    console.log(`Please put a number to the corresponding ${field}`)
+    console.log(`Please put a number to your ${field}`)
 }
 
 function printSus() {
     let startNum = parseInt(startInput.value);
     let divNum = parseInt(divInput.value);
 
-    for (let i = startNum, div = divNum, range = addRange(startNum); 
-    i < range; i++) {
-        if (i % div === 0) {
-            console.error(i);
-        } else {
-            console.log(i);
+    if (typeof startNum !== "number") {
+        showWarning("starting number")
+    } else if (typeof divNum !== "number") {
+        showWarning("divisior number")
+    } else {
+        for (let i = startNum, div = divNum, range = addRange(startNum); 
+        i < range; i++) {
+            if (i % div === 0) {
+                console.error(i);
+            } else {
+                console.log(i);
+            }
         }
     }
+
 }
