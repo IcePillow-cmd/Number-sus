45d27edc1215244ce5c631dc8ee6641024ab0dce
ColdPillow <poplizardo4321@gmail.com>
2022-06-15 18:49:35 +0800

    Added prototype of printSus function

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ const divInput = document.querySelector("#div-input");
 function addRange(start, range=10) {
     return start + range;
 }
+
+function printSus() {
+    let startNum = parseInt(startInput.value);
+    let divNum = parseInt(divInput.value);
+    
+    for (let i = startNum, div = divNum, range = addRange(startNum); 
+    i < range; i++) {
+        if (i % div === 0) {
+            console.error(i);
+        } else {
+            console.log(i);
+        }
+    }
+}
