d431ba8189f3a0eefbfb7b67b46adaba9cf51334
ColdPillow <poplizardo4321@gmail.com>
2022-06-15 18:07:26 +0800

    Added content to javascript
    - Added script tag to HTML
    - Added document variables
    - Added addRange function

---
+++ main.js
@@ -0,0 +1,6 @@
+const startInput = document.querySelector("#start-input");
+const divInput = document.querySelector("#div-input");
+
+function addRange(start, range=10) {
+    return start + range;
+}
